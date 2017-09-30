17c174a044907d0b28d8bba31afcbf44c27b6fa6
lepon01 <leondrolio@gmail.com>
2017-09-30 20:58:30 +0100

    Put URL on server side

--- gm_dectalk/lua/autorun/client/client.lua
+++ gm_dectalk/lua/autorun/client/client.lua
@@ -2,23 +2,21 @@ g_station = nil;
 
 --Recieve ChatTaunts
 net.Receive( "dectalk", function()
-	print("[DECtalk] Data recieved!")
-	local text = url_encode(net.ReadString())
-	--local text = net.ReadString()
-	local disk = net.ReadBool()
 	if ("0" == file.Read("tts.txt", "DATA")) then
 		return print("[DECtalk] Blocked DECtalk. To enable, type `tts_enable`")
 	end
 
-	print("http://talk.moustacheminer.com/api/gen/?dectalk=" .. text)
-	sound.PlayURL("http://talk.moustacheminer.com/api/gen/?dectalk=" .. text, "", function(station, id, msg)
-		print(id)
-		print(msg)
+	local text = url_encode(net.ReadString())
+
+	local disk = net.ReadBool()
+
+	sound.PlayURL(text, "", function(station, id, msg)
 		if ( IsValid( station ) ) then
 			station:Play()
+			-- Don't allow the Trash Compactor to clear out station just yet, fixes cutting out.
 			g_station = station
 		else
-			LocalPlayer():ChatPrint("Invalid URL!")
+			print("[DECtalk] Could not request " .. text .. ".")
 		end
 	end )
 
@@ -32,12 +30,4 @@ concommand.Add( "tts_enable", function()
 	file.Write("tts.txt", "1")
 end )
 
-function url_encode(str)
-	if (str) then
-		str = string.gsub (str, "\n", "\r\n")
-		str = string.gsub (str, "([^%w %-%_%.%~])",
-		function (c) return string.format ("%%%02X", string.byte(c)) end)
-		str = string.gsub (str, " ", "+")
-	end
-	return str
-end
+
--- gm_dectalk/lua/autorun/server/server.lua
+++ gm_dectalk/lua/autorun/server/server.lua
@@ -1,22 +1,29 @@
+url = 'http://talk.moustacheminer.com'
+
 util.AddNetworkString('dectalk')
 
-function senddectalk(talk)
+hook.Add( 'PlayerSay', 'dectalk', function( ply, text, teamchat )
 	net.Start('dectalk')
-	net.WriteString(talk)
-	net.Broadcast()
-end
+	net.WriteString(url .. "/api/gen/?dectalk=" .. url_encode(text))
 
-hook.Add( "PlayerSay", "dectalk", function( ply, text, team )
-	if !ply.cooldown or ply:IsAdmin() then
-		senddectalk(text)
-
-		-- Set the cooldown flag
-		ply.cooldown = true
-
-		-- Disable the cooldown after 5 seconds
-		timer.Simple(5, function()
-			ply.cooldown = false
-		end )
+	if teamchat then
+		net.Send(team.GetPlayers(ply:Team()))
+	else
+		net.Broadcast()
 	end
 end
 )
+
+concommand.Add( 'tts_url', function(ply, cmd, args, argStr)
+	url = argStr
+end )
+
+function url_encode(str)
+	if (str) then
+		str = string.gsub (str, "\n", "\r\n")
+		str = string.gsub (str, "([^%w %-%_%.%~])",
+		function (c) return string.format ("%%%02X", string.byte(c)) end)
+		str = string.gsub (str, " ", "+")
+	end
+	return str
+end
